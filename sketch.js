92f47180be850df41857818ace68e54ef31c1112
EvilRaptor1997 <151682309+EvilRaptor1997@users.noreply.github.com>
2024-07-17 04:49:14 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,25 @@
 // sketch.js
+
+const tiles = [];
+
+function preload() {
+    tiles[0] = loadImage("blank.png");
+    tiles[1] = loadImage("up.png");
+    tiles[2] = loadImage("right.png");
+    tiles[3] = loadImage("down.png");
+    tiles[4] = loadImage("left.png");
+}
+
+
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 600);
 }
 
 function draw() {
     background(0);
+    for (let i = 0; i < 4; i++) {
+        image(tiles[i], i * 50, 0);
+    }
+    image(tiles[4], 0, 50);
 }
